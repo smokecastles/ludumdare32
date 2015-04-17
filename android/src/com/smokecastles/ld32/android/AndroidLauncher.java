de16e4cf5e8077e9d10555fa26db6d689167f4ab
Juanma Reyes <juanma.resp@gmail.com>
2015-04-18 00:08:48 +0200

    Initial commit (new libgdx project)

---
+++ android/src/com/smokecastles/ld32/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.smokecastles.ld32.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.smokecastles.ld32.LD32Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD32Game(), config);
+	}
+}
---
+++ desktop/src/com/smokecastles/ld32/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smokecastles.ld32.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smokecastles.ld32.LD32Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD32Game(), config);
+	}
+}
