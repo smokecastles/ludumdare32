3293e5ab624a3f9ff4afd0667e37509b19d3ad22
zaidolbap <maildpablo@gmail.com>
2015-04-18 22:45:20 +0200

    HUD first steps

--- core/src/com/smokecastles/ld32/utils/Assets.java
+++ core/src/com/smokecastles/ld32/utils/Assets.java
@@ -9,10 +9,14 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  */
 public class Assets {
     public static TextureRegion playerNormal;
+    public static TextureRegion player_life_unit;
+    public static TextureRegion player_life_unit2;
 
     public static void load() {
-        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("textures.atlas"));
+        TextureAtlas atlas  = new TextureAtlas(Gdx.files.internal("textures.atlas"));
 
-        playerNormal   = new TextureRegion(atlas.findRegion("char2_blue"));
+        playerNormal        = new TextureRegion(atlas.findRegion("char2_blue"));
+        player_life_unit    = new TextureRegion(atlas.findRegion("foe1_red"));
+        player_life_unit2    = new TextureRegion(atlas.findRegion("foe1_blue"));
     }
 }
--- core/src/com/smokecastles/ld32/utils/Constants.java
+++ core/src/com/smokecastles/ld32/utils/Constants.java
@@ -6,14 +6,15 @@ package com.smokecastles.ld32.utils;
 public abstract class Constants {
     public static String GAME_TITLE = "Ludum Dare 32: An Unconventional Weapon";
 
-    public static int NATIVE_WIDTH = 1280;
+    public static int NATIVE_WIDTH  = 1280;
     public static int NATIVE_HEIGHT = 720;
 
-    public static int WINDOW_WIDTH = NATIVE_WIDTH;
+    public static int WINDOW_WIDTH  = NATIVE_WIDTH;
     public static int WINDOW_HEIGHT = NATIVE_HEIGHT;
+    public static int tile_side     = 30; // px
 
-    public static float BGCOLOR_R = 0.204f;
-    public static float BGCOLOR_G = 0.204f;
-    public static float BGCOLOR_B = 0.467f;
+    public static float BGCOLOR_R   = 0.204f;
+    public static float BGCOLOR_G   = 0.204f;
+    public static float BGCOLOR_B   = 0.467f;
 
 }
